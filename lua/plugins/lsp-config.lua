172bc8b1653badf10957abd5dc25e990d1bb2cdc
maucontreras_meli <mauricio.contreras@mercadolibre.com.co>
2025-09-24 17:05:22 -0500

    fix: resolve nvim-lspconfig deprecation warning
    Replace require('lspconfig') usage with vim.lsp.config syntax
    to eliminate deprecation warnings in Neovim 0.11+

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -10,50 +10,10 @@ return {
 		config = function()
 			require("mason-lspconfig").setup({
 				ensure_installed = {
-					-- Lua
-					"lua_ls",
-
-					-- Shell
-					"bashls",
-
-					-- Docker
-					"dockerls",
-					"docker_compose_language_service",
-
-					-- Go
-					"gopls",
-
-					-- Java
-					"jdtls",
-
-					-- JavaScript/TypeScript
-					"ts_ls", -- Main TypeScript/JavaScript server
-					"eslint",
-
-					-- JSON
-					"jsonls",
-
-					-- Python
-					"pyright",
-					"ruff", -- Linter for Python
-
-					-- Rust
-					"rust_analyzer",
-
-					-- SQL
-					"sqlls",
-
-					-- Terraform
-					"terraformls",
-
-					-- TOML
-					"taplo",
-
-					-- XML
-					"lemminx",
-
-					-- YAML
-					"yamlls",
+					"lua_ls", "bashls", "dockerls", "docker_compose_language_service",
+					"gopls", "jdtls", "ts_ls", "eslint", "jsonls", "pyright",
+					"ruff", "rust_analyzer", "sqlls", "terraformls", "taplo",
+					"lemminx", "yamlls",
 				},
 			})
 		end,
@@ -62,110 +22,64 @@ return {
 		"neovim/nvim-lspconfig",
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
-			-- Global mappings.
-			-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+
+			-- Use the new vim.lsp.config syntax (Neovim 0.11+)
+			local servers = {
+				"lua_ls", "bashls", "dockerls", "docker_compose_language_service",
+				"gopls", "jdtls", "ts_ls", "eslint", "jsonls", "pyright",
+				"ruff", "rust_analyzer", "sqlls", "terraformls", "taplo",
+				"lemminx", "yamlls"
+			}
+
+			for _, server in ipairs(servers) do
+				vim.lsp.config[server] = {
+					capabilities = capabilities,
+				}
+			end
+
+			-- Custom configuration for lua_ls
+			vim.lsp.config.lua_ls = {
+				capabilities = capabilities,
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim" },
+						},
+					},
+				},
+			}
+
+			-- Global mappings for diagnostics
 			vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
 			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
 			vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
 			vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
 
-			-- Use LspAttach autocommand to only map the following keys
-			-- after the language server attaches to the current buffer
+			-- LSP keymaps when server attaches
 			vim.api.nvim_create_autocmd("LspAttach", {
 				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
 				callback = function(ev)
-					-- Enable completion triggered by <c-x><c-o>
 					vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-					-- Buffer local mappings.
-					-- See `:help vim.lsp.*` for documentation on any of the below functions
-
-					-- Go to definition, declaration, implementation, etc.
-					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
-					vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-					vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
-					vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, {})
-					vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, {})
-					vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, {})
+					-- Buffer-local keymaps
+					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = ev.buf })
+					vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = ev.buf })
+					vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = ev.buf })
+					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = ev.buf })
+					vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, { buffer = ev.buf })
+					vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, { buffer = ev.buf })
+					vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, { buffer = ev.buf })
 					vim.keymap.set("n", "<leader>wl", function()
 						print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-					end, {})
-					vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, {})
-
-					-- Renaming and code actions
-					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
-					vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-
-					-- References, formatting
-					vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+					end, { buffer = ev.buf })
+					vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, { buffer = ev.buf })
+					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = ev.buf })
+					vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { buffer = ev.buf })
+					vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = ev.buf })
 					vim.keymap.set("n", "<leader>f", function()
 						vim.lsp.buf.format({ async = true })
-					end, {})
+					end, { buffer = ev.buf })
 				end,
-			})
-
-			-- Setup all servers from the list above
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.bashls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.dockerls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.docker_compose_language_service.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.gopls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.jdtls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.ts_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.eslint.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.jsonls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.pyright.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.ruff.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.rust_analyzer.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.sqlls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.terraformls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.taplo.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.lemminx.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.yamlls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.html.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.jsonls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.lemminx.setup({
-				capabilities = capabilities,
 			})
 		end,
 	},
